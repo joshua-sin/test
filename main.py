5f6f771f8afc27782b650cae7535a03291e8e35f
joshua-sin <113292775+joshua-sin@users.noreply.github.com>
2023-01-18 17:27:25 +0800

    Add files via upload
    Test

---
+++ main.py
@@ -0,0 +1,13 @@
+from BlogApp import app
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True) 
+    
+'''
+Work left to do;
+    1. Add modal box for anouncements in home page
+    2. Send to joshua to check pfp management and add img handling system to the merch store
+'''
+
+
